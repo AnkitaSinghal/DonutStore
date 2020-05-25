1edffa9550e6842482ece460b3451a56872854bf
Ankita Singhal <AnkitaSinghal@users.noreply.github.com>
2020-05-25 11:23:48 +0200

    initial setup with dynamic route for donut

---
+++ scully.DonutStore.config.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const {registerPlugin} = require('@scullyio/scully');
+
+// registerPlugin('router', 'thinksterDonutPlugin', async() => {
+//     return [{ route: '/donuts/1'}, {route: '/donuts/2'}, {route: '/donuts/3'}];
+// })
+
+
+// exports.config = {
+//     projectRoot: "./src",
+//     projectName: "DonutStore",
+//     outDir: './dist/static',
+//     routes: {
+//         '/donuts/:donutId': {
+//             type: 'thinksterDonutPlugin'
+//         }
+//     }
+// };
+
+exports.config = {
+    projectRoot: "./src",
+    projectName: "DonutStore",
+    outDir: './dist/static',
+    routes: {
+        // This here gets the json and using property if creates the routes (intresting!)
+        '/donuts/:donutId': {
+            type: 'json',
+            donutId: {
+                url: 'http://localhost:3000/donuts',
+                property:'id'
+            }
+        }
+    }
+};
